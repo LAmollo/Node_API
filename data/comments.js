731fd531f6d6c074159db0eb5e6d2670295e2e71
LAmollo <lawinyamuga@gmail.com>
2024-06-11 16:43:01 -0700

    updates...comments

--- data/comments.js
+++ data/comments.js
@@ -1,5 +1,19 @@
 // comments.js
-
-const comments = [];
-
-module.exports = comments;
+const comments = [
+    {
+      id: 1,
+      userId: 2,
+      postId: 1,
+      body: "This is a comment on the first post."
+    },
+    {
+      id: 2,
+      userId: 1,
+      postId: 2,
+      body: "This is a comment on the second post."
+    },
+    // More comments if needed
+  ];
+  
+  module.exports = comments;
+  
